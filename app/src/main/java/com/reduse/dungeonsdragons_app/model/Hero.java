9fd2b6fc2100a2e4e84282b9957410847118641b
Reduse <den.reduse@gmail.com>
2020-01-19 14:33:18 +0200

    Main model

--- app/src/main/java/com/reduse/dungeonsdragons_app/model/Hero.java
+++ app/src/main/java/com/reduse/dungeonsdragons_app/model/Hero.java
@@ -1,4 +1,133 @@
 package com.reduse.dungeonsdragons_app.model;
 
+import java.util.UUID;
+
 public class Hero {
+    private UUID id;
+    private String heroName;
+
+    private Integer speed;
+    private Integer Shild;
+
+    private Integer level;
+
+    private Integer power;
+
+    private Integer dexterity;
+
+    private Integer charisma;
+
+    private Integer body;
+
+    private Integer intelligence;
+
+    private Integer wisdom;
+
+    private String race;
+
+    private String classHero;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    public Integer getShild() {
+        return Shild;
+    }
+
+    public void setShild(Integer shild) {
+        Shild = shild;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public Integer getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(Integer dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public Integer getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(Integer charisma) {
+        this.charisma = charisma;
+    }
+
+    public Integer getBody() {
+        return body;
+    }
+
+    public void setBody(Integer body) {
+        this.body = body;
+    }
+
+    public Integer getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(Integer intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public Integer getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(Integer wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getClassHero() {
+        return classHero;
+    }
+
+    public void setClassHero(String classHero) {
+        this.classHero = classHero;
+    }
 }
